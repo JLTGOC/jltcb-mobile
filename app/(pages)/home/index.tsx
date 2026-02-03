f7f3d93bd664fda920eac2ffe4a6f3115e29102b
rndall <96964503+rndall@users.noreply.github.com>
2026-02-03 11:22:10 +0800

    :wrench: Refactor government logos marquee component

--- app/(pages)/home/index.tsx
+++ app/(pages)/home/index.tsx
@@ -1,14 +1,14 @@
-import { View } from "react-native";
+import GovernmentLogosMarquee from "@/src/components/home-section/GovernmentLogosMarquee";
+import NewsUpdates from "@/src/components/home-section/news-updates/NewsUpdatesContainer";
 import Reels from "@/src/components/home-section/reels/ReelsContainer";
-import GovermentLogos from "../../../src/components/home-section/GovernmentLogos";
-import NewsUpdates from "../../../src/components/home-section/news-updates/NewsUpdatesContainer";
+import { View } from "react-native";
 
 export default function Index() {
-	return (
-		<View style={{ backgroundColor: "#fff" }}>
-			<GovermentLogos />
-			<Reels />
-			<NewsUpdates />
-		</View>
-	);
+  return (
+    <View style={{ backgroundColor: "#fff" }}>
+      <GovernmentLogosMarquee />
+      <Reels />
+      <NewsUpdates />
+    </View>
+  );
 }
