9737b532b11289e595f7615bc050c0e8f09e58e5
khatejang <aguarinmarjorie@gmail.com>
2026-02-04 12:16:12 +0800

    :refactor about us guest
    change the description

--- app/(pages)/about-us/index.tsx
+++ app/(pages)/about-us/index.tsx
@@ -1,49 +1,52 @@
 import { Image } from "expo-image";
 import { ScrollView, Text, View } from "react-native";
-import CardTemplate2 from "@/src/components/about-section/CoreValuesTemplate";
-import Pillars from "@/src/components/about-section/PrincipleList";
+import CoreValuesTemplate from "@/src/components/about-section/CoreValuesTemplate";
+import PrincipleList from "@/src/components/about-section/PrincipleList";
 import styles from "./indexStyles";
 
 export default function Index() {
-	return (
-		<ScrollView style={styles.container}>
-			<Image
-				source={require("../../../src/assets/about_us/header.png")}
-				style={styles.imageHeader}
-			/>
-			<Text
-				style={{
-					fontSize: 12,
-					textAlign: "justify",
-					width: "100%",
-					paddingVertical: 10,
-					color: "#404040",
-					lineHeight: 14,
-				}}
-				allowFontScaling={false}
-			>
-				Proudly recognized as the first customs brokerage in the Philippines
-				certified compliant by the Professional Regulation Commission (PRC) and
-				the Professional Regulatory Board for Customs Brokers (PRBCB), we uphold
-				the highest standards of compliance, ethics, and professionalism in
-				every transaction.
-			</Text>
-			<CardTemplate2 />
-			<View style={{ paddingHorizontal: 10 }}>
-				<Text
-					numberOfLines={2}
-					style={{
-						fontSize: 25,
-						fontWeight: "bold",
-						color: "#EE9034",
-						marginVertical: 10,
-					}}
-					allowFontScaling={false}
-				>
-					OUR SERVICES ARE BUILT ON THREE PILLARS:
-				</Text>
-				<Pillars />
-			</View>
-		</ScrollView>
-	);
+  return (
+    <ScrollView style={styles.container}>
+      <Image
+        source={require("../../../src/assets/about_us/header.png")}
+        style={styles.imageHeader}
+      />
+      <Text
+        style={{
+          fontSize: 12,
+          textAlign: "justify",
+          width: "100%",
+          paddingVertical: 10,
+          color: "#404040",
+          lineHeight: 14,
+        }}
+        allowFontScaling={false}
+      >
+        Jill L. Tolentino Customs Brokerage (JLTCB) is a trusted and
+        fast-growing customs brokerage and logistics firm in the Philippines.
+        Built on strong regulatory expertise and ethical practice, we deliver
+        reliable, precision-driven trade solutions to local and international
+        clients. Led by nationally recognized customs and logistics leader 
+		Jill L. Tolentino, JLTCB is known for operational excellence, strict
+        compliance, and a client-first approach—helping businesses move goods
+        efficiently and with confidence.
+      </Text>
+      <CoreValuesTemplate />
+      <View style={{ paddingHorizontal: 10 }}>
+        <Text
+          numberOfLines={2}
+          style={{
+            fontSize: 25,
+            fontWeight: "bold",
+            color: "#EE9034",
+            marginVertical: 10,
+          }}
+          allowFontScaling={false}
+        >
+          OUR SERVICES ARE BUILT ON THREE PILLARS:
+        </Text>
+        <PrincipleList />
+      </View>
+    </ScrollView>
+  );
 }
--- src/components/about-section/CoreValuesTemplate.tsx
+++ src/components/about-section/CoreValuesTemplate.tsx
@@ -4,117 +4,124 @@ import { Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
 import { Card } from "react-native-paper";
 
 const SCREEN_WIDTH = Dimensions.get("window").width;
+const CARD_WIDTH = SCREEN_WIDTH * 0.8;
+const GAP = 20;
 export default function CoreValuesTemplate() {
-	const [activeIndex, setActiveIndex] = useState(0);
+  const [activeIndex, setActiveIndex] = useState(0);
 
-	const data = [
-		{
-			image: require("../../../src/assets/about_us/mission.png"),
-			title: "Mission",
-			description:
-				"To deliver world-class, compliant and value-driven customs brokerage and logistics solutions that empower businesses, elevate professionals, and advance a secure, transparent and globally competitive trade industry.",
-		},
-		{
-			image: require("../../../src/assets/about_us/vision.png"),
-			title: "Vision",
-			description:
-				"To be the world’s leading customs brokerage firm, trusted for client satisfaction, operational excellence, and integrity, while fostering a culture of growth, innovation, and partnership that advances both the profession and global trade.",
-		},
-		{
-			image: require("../../../src/assets/about_us/coreValues.png"),
-			title: "Core Values",
-			description:
-				"• Deliver Excellence Always\n• Lead with Integrity\n• Innovate with Purpose\n• Empower our People\n• Commit to Compliance\n• Put our Clients First\n• Serve Beyond Business",
-		},
-	];
+  const data = [
+    {
+      image: require("../../../src/assets/about_us/mission.png"),
+      title: "Mission",
+      description:
+        "To deliver world-class, compliant and value-driven customs brokerage and logistics solutions that empower businesses, elevate professionals, and advance a secure, transparent and globally competitive trade industry.",
+    },
+    {
+      image: require("../../../src/assets/about_us/vision.png"),
+      title: "Vision",
+      description:
+        "To be the world’s leading customs brokerage firm, trusted for client satisfaction, operational excellence, and integrity, while fostering a culture of growth, innovation, and partnership that advances both the profession and global trade.",
+    },
+    {
+      image: require("../../../src/assets/about_us/coreValues.png"),
+      title: "Core Values",
+      description:
+        "• Deliver Excellence Always\n• Lead with Integrity\n• Innovate with Purpose\n• Empower our People\n• Commit to Compliance\n• Put our Clients First\n• Serve Beyond Business",
+    },
+  ];
 
-	const handleScroll = (event: any) => {
-		const scrollX = event.nativeEvent.contentOffset.x;
-		const index = Math.round(scrollX / SCREEN_WIDTH);
-		setActiveIndex(index);
-	};
+  const handleScroll = (event: any) => {
+    const scrollX = event.nativeEvent.contentOffset.x;
+    const index = Math.round(scrollX / (CARD_WIDTH + GAP));
+    setActiveIndex(index);
+  };
 
-	return (
-		<View>
-			<ScrollView
-				horizontal
-				pagingEnabled
-				showsHorizontalScrollIndicator={false}
-				onScroll={handleScroll}
-				scrollEventThrottle={0}
-				contentContainerStyle={{ columnGap: 30, paddingHorizontal: 18 }}
-			>
-				{data.map((item, i) => (
-					<Card style={styles.card} mode="contained" key={i}>
-						<View
-							style={{
-								flexDirection: "row",
-								alignItems: "center",
-								justifyContent: "center",
-							}}
-						>
-							<Image source={item.image} style={{ width: 100, height: 100 }} />
-							<View style={{ flex: 1 }}>
-								<Text
-									style={{ fontSize: 22, fontWeight: "bold", color: "#161F3C" }}
-									allowFontScaling={false}
-								>
-									{item.title}
-								</Text>
-								<Text
-									style={{
-										fontSize: 12,
-										textAlign: "justify",
-										color: "#161F3C",
-										lineHeight: 14,
-									}}
-									allowFontScaling={false}
-								>
-									{item.description}
-								</Text>
-							</View>
-						</View>
-					</Card>
-				))}
-			</ScrollView>
+  return (
+    <View>
+      <ScrollView
+        horizontal
+        pagingEnabled={false}
+        snapToInterval={CARD_WIDTH + GAP}
+        showsHorizontalScrollIndicator={false}
+        onScroll={handleScroll}
+        scrollEventThrottle={0}
+        contentContainerStyle={{
+          columnGap: 30,
+          paddingHorizontal: (SCREEN_WIDTH - CARD_WIDTH) / 2,
+        }}
+      >
+        {data.map((item, i) => (
+          <Card style={styles.card} mode="contained" key={i}>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Image source={item.image} style={{ width: 100, height: 100 }} />
+              <View style={{ flex: 1 }}>
+                <Text
+                  style={{ fontSize: 22, fontWeight: "bold", color: "#161F3C" }}
+                  allowFontScaling={false}
+                >
+                  {item.title}
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 12,
+                    textAlign: "justify",
+                    color: "#161F3C",
+                    lineHeight: 14,
+                  }}
+                  allowFontScaling={false}
+                >
+                  {item.description}
+                </Text>
+              </View>
+            </View>
+          </Card>
+        ))}
+      </ScrollView>
 
-			{/* Indicator Lines */}
-			<View style={styles.indicatorContainer}>
-				{data.map((_, index) => (
-					<View
-						key={index}
-						style={[
-							styles.indicatorLine,
-							{
-								backgroundColor:
-									index === activeIndex ? "#aa5c0eff" : "#EE9034",
-							},
-						]}
-					/>
-				))}
-			</View>
-		</View>
-	);
+      {/* Indicator Lines */}
+      <View style={styles.indicatorContainer}>
+        {data.map((_, index) => (
+          <View
+            key={index}
+            style={[
+              styles.indicatorLine,
+              {
+                backgroundColor:
+                  index === activeIndex ? "#aa5c0eff" : "#EE9034",
+              },
+            ]}
+          />
+        ))}
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		paddingHorizontal: 20,
-		justifyContent: "space-between",
-	},
-	card: {
-		width: 300,
-		padding: 10,
-	},
-	indicatorContainer: {
-		flexDirection: "row",
-		justifyContent: "center",
-		marginTop: 10,
-	},
-	indicatorLine: {
-		width: 30,
-		height: 3,
-		marginHorizontal: 5,
-		borderRadius: 2,
-	},
+  container: {
+    paddingHorizontal: 20,
+    justifyContent: "space-between",
+  },
+  card: {
+    width: 300,
+    padding: 10,
+	elevation: 2,
+  },
+  indicatorContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 10,
+  },
+  indicatorLine: {
+    width: 30,
+    height: 3,
+    marginHorizontal: 5,
+    borderRadius: 2,
+  },
 });
