083baaffb7f70376eae184075f7ded5579499942
rndall <96964503+rndall@users.noreply.github.com>
2026-02-06 13:54:51 +0800

    :wrench: Fetch and show user dashboard folder count

--- src/components/dashboard-section/FolderButton.tsx
+++ src/components/dashboard-section/FolderButton.tsx
@@ -1,59 +1,81 @@
-import type { DashbordFolderItem } from "@/src/types/dashboard";
 import { Image, ImageBackground } from "expo-image";
 import {
-  StyleSheet,
-  TouchableOpacity,
-  type TouchableOpacityProps,
+	StyleSheet,
+	TouchableOpacity,
+	type TouchableOpacityProps,
+	View,
 } from "react-native";
 import { Text } from "react-native-paper";
+import type { DashbordFolderItem } from "@/src/types/dashboard";
 
 const FOLDER_BACKGROUNDS = {
-  dark: require("../../assets/folders/dark.png"),
-  light: require("../../assets/folders/light.png"),
+	dark: require("../../assets/folders/dark.png"),
+	light: require("../../assets/folders/light.png"),
 } as const;
 
 type FolderButtonProps = {
-  folder: DashbordFolderItem;
-  variant: "light" | "dark";
+	folder: DashbordFolderItem;
+	variant: "light" | "dark";
 };
 
 export default function FolderButton({
-  folder,
-  variant,
-  ...props
+	folder,
+	variant,
+	...props
 }: FolderButtonProps & TouchableOpacityProps) {
-  return (
-    <TouchableOpacity {...props} style={styles.folderBtn}>
-      <ImageBackground
-        source={FOLDER_BACKGROUNDS[variant]}
-        style={styles.imageBackground}
-        contentFit="contain"
-      >
-        <Image
-          source={{ uri: folder.icon }}
-          style={folder.iconStyles ?? { aspectRatio: 2823 / 2013, height: 30 }}
-        />
-        <Text style={styles.folderText} variant="labelMedium">
-          {folder.title}
-        </Text>
-      </ImageBackground>
-    </TouchableOpacity>
-  );
+	return (
+		<TouchableOpacity {...props} style={styles.folderBtn}>
+			<ImageBackground
+				source={FOLDER_BACKGROUNDS[variant]}
+				style={styles.imageBackground}
+				contentFit="contain"
+			>
+				<View style={styles.folderContent}>
+					<Image
+						source={{ uri: folder.icon }}
+						style={[
+							folder.iconStyles ?? { aspectRatio: 2823 / 2013, height: 30 },
+							{
+								alignSelf: "center",
+							},
+						]}
+					/>
+					<View style={styles.folderTextWrapper}>
+						<Text style={styles.folderText} variant="labelMedium">
+							{folder.title}
+						</Text>
+						<Text style={styles.folderText} variant="labelMedium">
+							{folder.count}
+						</Text>
+					</View>
+				</View>
+			</ImageBackground>
+		</TouchableOpacity>
+	);
 }
 
 const styles = StyleSheet.create({
-  folderBtn: {
-    width: "33.3%",
-    alignItems: "center",
-  },
-  imageBackground: {
-    width: "100%",
-    aspectRatio: 111 / 82,
-    justifyContent: "center",
-    alignItems: "center",
-    gap: 4,
-  },
-  folderText: {
-    textTransform: "uppercase",
-  },
+	folderBtn: {
+		width: "33.3%",
+		alignItems: "center",
+	},
+	imageBackground: {
+		width: "100%",
+		aspectRatio: 111 / 82,
+		paddingTop: "17%",
+		paddingBottom: "11%",
+		paddingHorizontal: "12%",
+		gap: 4,
+	},
+	folderText: {
+		textTransform: "uppercase",
+	},
+	folderContent: {
+		flex: 1,
+		justifyContent: "space-between",
+	},
+	folderTextWrapper: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+	},
 });
--- src/constants/user-dashboards.ts
+++ src/constants/user-dashboards.ts
@@ -1,111 +1,137 @@
-import type { DashboardFolderSection } from "../types/dashboard";
+import type {
+	AccountSpecialistDashboard,
+	ClientDashboard,
+	DashboardFolderSection,
+} from "../types/dashboard";
 
-export const CLIENT_DB_FOLDER_SECTIONS: DashboardFolderSection[] = [
-  {
-    title: "Shipment",
-    data: [
-      {
-        title: "Ongoing",
-        icon: "ongoing",
-        href: "/(client)/dashboard",
-      },
-      {
-        title: "Completed",
-        icon: "delivered",
-        href: "/(client)/dashboard",
-      },
-    ],
-  },
-  {
-    title: "Quotation",
-    data: [
-      {
-        title: "Requested",
-        icon: "request-quotation",
-        href: "/(client)/dashboard",
-      },
-      {
-        title: "Responded",
-        icon: "quotations",
-        href: "/(client)/dashboard",
-      },
-    ],
-  },
-] as const;
+export const CLIENT_DB_FOLDER_SECTIONS: DashboardFolderSection<ClientDashboard>[] =
+	[
+		{
+			title: "Shipment",
+			sectionKey: "shipments",
+			data: [
+				{
+					title: "Ongoing",
+					countKey: "ongoing_count",
+					icon: "ongoing",
+					href: "/(client)/dashboard",
+				},
+				{
+					title: "Completed",
+					countKey: "completed_count",
+					icon: "delivered",
+					href: "/(client)/dashboard",
+				},
+			],
+		},
+		{
+			title: "Quotation",
+			sectionKey: "quotations",
+			data: [
+				{
+					title: "Requested",
+					countKey: "requested_count",
+					icon: "request-quotation",
+					href: "/(client)/dashboard",
+				},
+				{
+					title: "Responded",
+					countKey: "responded_count",
+					icon: "quotations",
+					href: "/(client)/dashboard",
+				},
+			],
+		},
+	] as const;
 
-export const AS_DB_FOLDER_SECTIONS: DashboardFolderSection[] = [
-  {
-    title: "Leads",
-    data: [
-      {
-        title: "Queries",
-        icon: "delivered",
-        href: "/(employee-account-specialist)/dashboard",
-      },
-      {
-        title: "New",
-        icon: "ongoing",
-        href: "/(employee-account-specialist)/dashboard",
-      },
-      {
-        title: "Replied",
-        icon: "ongoing",
-        href: "/(employee-account-specialist)/dashboard",
-      },
-    ],
-  },
-  {
-    title: "Shipment",
-    data: [
-      {
-        title: "Ongoing",
-        icon: "ongoing",
-        href: "/(employee-account-specialist)/dashboard",
-      },
-      {
-        title: "Delivered",
-        icon: "delivered",
-        href: "/(employee-account-specialist)/dashboard",
-      },
-    ],
-  },
-  {
-    title: "Quotations",
-    data: [
-      {
-        title: "New",
-        icon: "quotations",
-        href: "/(employee-account-specialist)/dashboard",
-      },
-      {
-        title: "Responded",
-        icon: "delivered",
-        href: "/(employee-account-specialist)/dashboard",
-      },
-      {
-        title: "Accepted",
-        icon: "ongoing",
-        href: "/(employee-account-specialist)/dashboard",
-      },
-      {
-        title: "Discarded",
-        icon: "ongoing",
-        href: "/(employee-account-specialist)/dashboard",
-      },
-    ],
-  },
-  {
-    title: "Accounts",
-    data: [
-      {
-        title: "Clients",
-        icon: "accounts",
-        iconStyles: {
-          aspectRatio: 23 / 28,
-          height: 26,
-        },
-        href: "/(employee-account-specialist)/dashboard",
-      },
-    ],
-  },
-] as const;
+export const AS_DB_FOLDER_SECTIONS: DashboardFolderSection<AccountSpecialistDashboard>[] =
+	[
+		{
+			title: "Leads",
+			sectionKey: "leads",
+			data: [
+				{
+					title: "Queries",
+					countKey: "queries_count",
+					icon: "delivered",
+					href: "/(employee-account-specialist)/dashboard",
+				},
+				{
+					title: "New",
+					countKey: "new_count",
+					icon: "ongoing",
+					href: "/(employee-account-specialist)/dashboard",
+				},
+				{
+					title: "Replied",
+					countKey: "replied_count",
+					icon: "ongoing",
+					href: "/(employee-account-specialist)/dashboard",
+				},
+			],
+		},
+		{
+			title: "Shipment",
+			sectionKey: "shipments",
+			data: [
+				{
+					title: "Ongoing",
+					countKey: "ongoing_count",
+					icon: "ongoing",
+					href: "/(employee-account-specialist)/dashboard",
+				},
+				{
+					title: "Delivered",
+					countKey: "delivered_count",
+					icon: "delivered",
+					href: "/(employee-account-specialist)/dashboard",
+				},
+			],
+		},
+		{
+			title: "Quotations",
+			sectionKey: "quotations",
+			data: [
+				{
+					title: "New",
+					countKey: "new_count",
+					icon: "quotations",
+					href: "/(employee-account-specialist)/dashboard",
+				},
+				{
+					title: "Responded",
+					countKey: "responded_count",
+					icon: "delivered",
+					href: "/(employee-account-specialist)/dashboard",
+				},
+				{
+					title: "Accepted",
+					countKey: "accepted_count",
+					icon: "ongoing",
+					href: "/(employee-account-specialist)/dashboard",
+				},
+				{
+					title: "Discarded",
+					countKey: "discarded_count",
+					icon: "ongoing",
+					href: "/(employee-account-specialist)/dashboard",
+				},
+			],
+		},
+		{
+			title: "Accounts",
+			sectionKey: "accounts",
+			data: [
+				{
+					title: "Clients",
+					countKey: "clients_count",
+					icon: "accounts",
+					iconStyles: {
+						aspectRatio: 23 / 28,
+						height: 26,
+					},
+					href: "/(employee-account-specialist)/dashboard",
+				},
+			],
+		},
+	] as const;
