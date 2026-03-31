44d965601a0d665236b0d21f6ba0506fd3f14444
rndall <96964503+rndall@users.noreply.github.com>
2026-03-31 15:49:15 +0800

    :wrench: Create UI for first part of shipment form

--- app/(employee-account-specialist)/(tabs)/dashboard/accepted-quotation/make-job-order/shipment.tsx
+++ app/(employee-account-specialist)/(tabs)/dashboard/accepted-quotation/make-job-order/shipment.tsx
@@ -6,7 +6,7 @@ import {
 	type TextStyle,
 	View,
 } from "react-native";
-import type { IAutocompleteDropdownProps } from "react-native-autocomplete-dropdown";
+import { Button } from "react-native-paper";
 import AutocompleteDropdown from "@/src/components/ui/AutocompleteDropdown";
 import BannerHeader from "@/src/components/ui/BannerHeader";
 import FieldLegend from "@/src/components/ui/FieldLegend";
@@ -27,8 +27,14 @@ export default function Shipment() {
 
 			<View style={styles.content}>
 				<FloatingLabelInput label="SUBJECT" />
-				<FloatingLabelInput label="EMAIL BODY" multiline numberOfLines={4} />
-				<FieldLegend>SERVICE INFORMATION</FieldLegend>
+				<FloatingLabelInput
+					label="EMAIL BODY"
+					multiline
+					numberOfLines={4}
+					style={{ minHeight: 65 }}
+				/>
+
+				<FieldLegend>CLIENT INFORMATION</FieldLegend>
 				<FloatingLabelInput label="CONSIGNEE" editable={false} />
 				<AutocompleteDropdown
 					textInputProps={{
@@ -50,9 +56,36 @@ export default function Shipment() {
 				<FloatingLabelInput
 					label="CLIENT TONE / ATTITUDE"
 					multiline
-					numberOfLines={1}
+					numberOfLines={2}
 				/>
-				<FloatingLabelInput label="SHIPPER" editable={false} />
+				<FloatingLabelInput
+					label="REMARKS ON HANDLING CLIENT"
+					multiline
+					numberOfLines={2}
+				/>
+
+				<FieldLegend>SERVICE INFORMATION</FieldLegend>
+				<AutocompleteDropdown
+					textInputProps={{
+						placeholder: "SERVICE LEVEL",
+						style: TEXT_INPUT_STYLES,
+					}}
+					suggestionsListTextStyle={{ color: TEXT_COLOR }}
+					dataSet={null}
+				/>
+				<FloatingLabelInput label="BL NO" />
+
+				<FieldLegend>IF COORDINATED:</FieldLegend>
+				<FloatingLabelInput label="ESTIMATED TIME OF ARRIVAL" />
+				<FloatingLabelInput label="ESTIMATED TIME OF DEPARTURE" />
+
+				<Button
+					theme={{ colors: { primary: "#1C213B" } }}
+					mode="contained"
+					style={styles.button}
+				>
+					NEXT
+				</Button>
 			</View>
 		</ScrollView>
 	);
@@ -61,6 +94,11 @@ export default function Shipment() {
 const styles = StyleSheet.create({
 	content: {
 		paddingHorizontal: 24,
+		paddingBottom: 20,
 		gap: 12,
+	},
+	button: {
+		borderRadius: 6,
+		paddingVertical: 4,
 	},
 });
