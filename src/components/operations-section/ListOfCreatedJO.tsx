2f8c0e8060a5417b5c4127f8bc41b6a7161ec40e
Khatejang <aguarinmarjorie@gmail.com>
2026-04-07 09:50:51 +0800

    : wrench - apply search in the UI of the created job order in the employee-operation side

--- src/components/operations-section/ListOfCreatedJO.tsx
+++ src/components/operations-section/ListOfCreatedJO.tsx
@@ -1,16 +1,25 @@
 import { createdJobOrdersQueryOptions } from "@/src/query-options/operations/jobOrder";
-import type { JobOrder } from "@/src/types/job-order";
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 import { FlatList, View } from "react-native";
 import { ActivityIndicator, Text } from "react-native-paper";
+
+import BannerHeader from "@/src/components/ui/BannerHeader";
+import Search from "../ui/Search";
+import SwitchToggle from "../ui/SwitchToggle";
 import JobOrderCardTemplate from "./JobOrderCardTemplate";
 
 export default function ListOfCreatedJO() {
+  const [search, setSearch] = useState<string>("");
+  const [submittedSearch, setSubmittedSearch] = useState("");
+
   const { data, isPending, error, refetch } = useQuery(
     createdJobOrdersQueryOptions({ status: "created" }),
   );
 
-  console.log("khate", data);
+  const handleSearch = () => {
+    setSubmittedSearch(search.trim());
+  };
 
   if (isPending) {
     return <ActivityIndicator style={{ marginTop: 16 }} />;
@@ -32,9 +41,27 @@ export default function ListOfCreatedJO() {
 
   return (
     <View style={{ flex: 1 }}>
+      <BannerHeader variant="light" title="List of Pending Job Orders" />
+
+      <Search
+        onSearch={handleSearch}
+        onChangeText={setSearch}
+        value={search}
+        placeholder="SEARCH QUOTATION"
+        autoCapitalize="none"
+        placeholderTextColor="black"
+        onSubmitEditing={handleSearch}
+        returnKeyType="search"
+        containerStyle={{
+          marginHorizontal: 16,
+          marginBottom: 20,
+          backgroundColor: "#F8F8F8",
+        }}
+      />
+
       <FlatList
         style={{ flex: 1 }}
-        data={data}
+        data={data ?? []}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
           <JobOrderCardTemplate
