8b3731d7c06c1ecbcb6351a344bc6133afc44efa
rndall <96964503+rndall@users.noreply.github.com>
2026-05-15 11:27:26 +0800

    :bug: Decode chat file names for display

--- src/components/chats-section/ChatFileCard.tsx
+++ src/components/chats-section/ChatFileCard.tsx
@@ -7,68 +7,68 @@ import { handleSaveFile } from "@/src/utils/handleFileDownload";
 import { showToast } from "@/src/utils/showToast";
 
 type Props = {
-	file: FileMessage;
+  file: FileMessage;
 };
 
 export default function ChatFileCard({ file }: Props) {
-	const { userData, token } = useAuth();
-	const isUserMessage = userData?.id === file.sender.id;
+  const { userData, token } = useAuth();
+  const isUserMessage = userData?.id === file.sender.id;
 
-	const handlePress = async () => {
-		try {
-			await handleSaveFile({
-				url: file.file_url,
-				token: token!,
-				cacheDir: "files",
-				fileName: file.file_name,
-			});
-		} catch (err) {
-			showToast("Failed to download file.");
-			console.error(err);
-		}
-	};
+  const handlePress = async () => {
+    try {
+      await handleSaveFile({
+        url: file.file_url,
+        token: token!,
+        cacheDir: "files",
+        fileName: decodeURI(file.file_name),
+      });
+    } catch (err) {
+      showToast("Failed to download file.");
+      console.error(err);
+    }
+  };
 
-	return (
-		<View
-			style={[
-				styles.container,
-				isUserMessage && { justifyContent: "flex-end" },
-			]}
-		>
-			<Card style={styles.card} mode="contained" onPress={handlePress}>
-				<Card.Content style={styles.content}>
-					<Avatar.Icon size={32} icon="file" />
-					<View style={styles.textContentContainer}>
-						<Text variant="labelLarge" numberOfLines={4}>
-							{file.file_name}
-						</Text>
-						{/* <Text variant="bodySmall" style={styles.fileSize}>
+  return (
+    <View
+      style={[
+        styles.container,
+        isUserMessage && { justifyContent: "flex-end" },
+      ]}
+    >
+      <Card style={styles.card} mode="contained" onPress={handlePress}>
+        <Card.Content style={styles.content}>
+          <Avatar.Icon size={32} icon="file" />
+          <View style={styles.textContentContainer}>
+            <Text variant="labelLarge" numberOfLines={4}>
+              {decodeURI(file.file_name)}
+            </Text>
+            {/* <Text variant="bodySmall" style={styles.fileSize}>
             {file.}
           </Text> */}
-					</View>
-				</Card.Content>
-			</Card>
-		</View>
-	);
+          </View>
+        </Card.Content>
+      </Card>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flexDirection: "row",
-	},
-	card: {
-		width: "100%",
-	},
-	content: {
-		flexDirection: "row",
-		gap: 12,
-	},
-	textContentContainer: { flex: 1, flexShrink: 1 },
-	fileSize: {
-		color: "gray",
-	},
-	iconContainerStyle: {
-		width: 32,
-		height: 32,
-	},
+  container: {
+    flexDirection: "row",
+  },
+  card: {
+    width: "100%",
+  },
+  content: {
+    flexDirection: "row",
+    gap: 12,
+  },
+  textContentContainer: { flex: 1, flexShrink: 1 },
+  fileSize: {
+    color: "gray",
+  },
+  iconContainerStyle: {
+    width: 32,
+    height: 32,
+  },
 });
