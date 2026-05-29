0aab831d059399e21ee3114657c298df72603454
rndall <96964503+rndall@users.noreply.github.com>
2026-05-29 16:49:21 +0800

    :wrench: Add @tanstack/eslint-plugin-query to ESLint configuration and package dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,11 @@
 const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
 const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+const pluginQuery = require("@tanstack/eslint-plugin-query");
 
 module.exports = defineConfig([
   expoConfig,
+  ...pluginQuery.configs["flat/recommended"],
   eslintPluginPrettierRecommended,
   {
     rules: {
